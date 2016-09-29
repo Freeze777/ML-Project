60210aa85387260cb77d1c3b96a37389679cbbec
Freeze777 <freezefrancis@gmail.com>
2016-09-29 13:02:37 +0530

    first approach

--- git_push.sh
+++ git_push.sh
@@ -1,9 +1,10 @@
 git add *
-git rm -r --cached *.pem
-git rm -r --cached *.pkl
-git rm -r --cached *.csv
-git rm -r --cached *.npy
-git rm -r --cached *.npz
-git rm -r --cached *.gl
+git rm -r --cached  dataset-train
+git rm -r --cached  *.pem
+git rm -r --cached  *.csv
+git rm -r --cached  *.pkl
+git rm -r --cached  *.npy
+git rm -r --cached  *.pyc
+git rm -r --cached  *.npz
 git commit -a
 git push https://github.com/Freeze777/ML-Project.git
